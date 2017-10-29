b24a0a0ec688bc9b4c456465e93776332bc4e719
Malkev Malkavian <Malkev Malkavian>
2017-10-29 21:10:21 +0100

    Initial

---
+++ src/components/Hello.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+.hello
+  h1 {{ msg }}
+  p
+    router-link(to='/') {{ $t('buttons.home') }}
+</template>
+
+<script>
+export default {
+  computed: {
+    msg () {
+      return this.$t('messages.hello', { name: this.name })
+    },
+    name () {
+      const name = this.$route.params.name
+      return name.charAt(0).toUpperCase() + name.slice(1)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus" scoped>
+h1
+  color green
+</style>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+div
+  p
+    | {{ $t('messages.welcome') }}
+  p
+    router-link(to='/hello/bourgeon') {{ $t('buttons.hello') }}
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
